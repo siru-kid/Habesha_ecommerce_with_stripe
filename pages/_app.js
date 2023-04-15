cf6bd9a596aee6a0259de13e66f531346ebe016f
sirak kidane <sikuanbesa123@gmail.com>
2023-04-15 16:18:40 +0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
---
+++ pages/success.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { BsBagCheckFill } from "react-icons/bs";
+import { useStateContext } from "../context/StateContext";
+import Link from "next/link";
+import { runFireworks } from "../lib/utils";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runFireworks();
+  }, []);
+  return (
+    <div className="sucess-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your emails inbox for the recipt</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:sirakkidane12345@gmail.com">
+            AzmarinoStores@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
